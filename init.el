ef91645a764d73f590cf9eac18c76f3624a590e4
tusharhero <tusharhero@sdf.org>
2023-08-27 21:51:23 +0530

    Switch to gruvbox🤓, also add theme cycling. Use helm-themes to checkout the themes.

--- init.el
+++ init.el
@@ -2,3 +2,22 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default))
+ '(helm-minibuffer-history-key "M-p")
+ '(org-format-latex-options
+   '(:foreground default :background default :scale 3 :html-foreground "Black" :html-background "Transparent" :html-scale 1 :matchers
+		 ("begin" "$1" "$" "$$" "\\(" "\\[")))
+ '(tramp-default-method "ssh")
+ '(tramp-default-user "tusharhero"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
