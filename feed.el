cddd96781d8ba37faa254afbb8b30f9c74b6a451
tusharhero <tusharhero@sdf.org>
2023-11-14 15:02:09 +0530

    disable org-roam and add some minor documentation

--- feed.el
+++ feed.el
@@ -2,4 +2,5 @@
 (custom-set-variables
  '(newsticker-url-list '(
 			 ("hackernews" "https://hnrss.org/frontpage" nil nil nil)
+			 ("archlinux news" "https://archlinux.org/feeds/news/" nil nil nil)
 			 )))
