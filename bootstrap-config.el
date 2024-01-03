cb9d895a65da3ecd0fa66eb0e7c3b96135d971cb
tusharhero <tusharhero@sdf.org>
2024-01-03 20:41:10 +0530

    Switch to manual tangling setup.
    Hence I removed all the useless files since they are not needed anymore.

---
+++ bootstrap-config.el
@@ -0,0 +1,8 @@
+#!/usr/bin/emacs --script
+;; Run this script to get 'bootstrap' ECFPAW from `config.org', of
+;; course the only requirement it has is Emacs is itself.
+(message "Loading Org...")
+(require 'org)
+(message "Tangling config.org...")
+(org-babel-tangle-file "config.org")
+(message "ECFPAW has been bootstrapped!")
