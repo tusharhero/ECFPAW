3f5a26383a74651cbd06b5b8db091d97fb36875e
tusharhero <tusharhero@sdf.org>
2025-01-14 10:18:01 +0530

    newsticker: remove feeds.el from git

--- feed.el
+++
@@ -1,6 +0,0 @@
-;; Put the feeds links here
-(custom-set-variables
- '(newsticker-url-list '(
-			 ("hackernews" "https://hnrss.org/frontpage" nil nil nil)
-			 ("archlinux news" "https://archlinux.org/feeds/news/" nil nil nil)
-			 )))
