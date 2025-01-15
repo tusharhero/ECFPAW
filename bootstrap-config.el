f303bcded38c43efcaa54bc77bd003e6833e37b1
tusharhero <tusharhero@sdf.org>
2025-01-15 16:47:50 +0530

    use bootstrap-config script to tangle on save

--- bootstrap-config.el
+++ bootstrap-config.el
@@ -1,8 +1,9 @@
-#!/usr/bin/env -S emacs --script
-;; Run this script to 'bootstrap' ECFPAW from `config.org', of
-;; course the only requirement it has is Emacs itself.
+#!/bin/env -S emacs --script
+;; Run this script to 'bootstrap' ECFPAW from `config.org', of course
+;; the only requirement it has is Emacs itself.
 (message "Loading Org...")
 (require 'org)
+(setopt org-confirm-babel-evaluate nil)
 (message "Tangling config.org...")
 (org-babel-tangle-file "config.org")
 (message "ECFPAW has been bootstrapped!")
